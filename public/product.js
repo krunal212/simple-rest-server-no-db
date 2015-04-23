c210f6449f9e1c9f2d1c0c8aaf1c982bc8577d25
krunal212 <krunal212@yahoo.com>
2015-04-23 17:38:39 -0400

    hi,product

--- public/product.js
+++ public/product.js
@@ -1,4 +1,19 @@
 $(document).ready(function(){
+	$.ajax({
+			url: "/products",
+			method: "get",
+			success: function(my){
+			console.log(my);
+			
+				for(i = 0; i < my.length; i++){
+					
+			$(".appe").append('<div id="'+my[i].id+'" class="jumbotron col-md-8"class="glyphicon glyphicon-remove pull-right"><button class="glyphicon glyphicon-remove pull-right delete"></button><button type="button" class="btn btn-primary productPrice pull-right">'+my[i].price+'</button><h3 class="col-md-6 productName">'+my[i].item+'</h3><h4 class="col-md-6 productDes">'+my[i].des+'</h4></div>');
+									
+				}
+					
+			}
+			
+	});
 	$(".add").on("click",function(event){
 		event.preventDefault();	
 		itemList={
@@ -8,13 +23,13 @@ $(document).ready(function(){
 		}	
 	
 			$.ajax({
-					url:"/users",
+					url:"/products",
 					type:"post",
 					data: itemList,
 					dataType:"json",
 					success:function(a){
 						var newJSON = a;
-						$(".appe").append('<div class="jumbotron col-md-8" id='+newJSON.id +'class="glyphicon glyphicon-remove pull-right"><button class="glyphicon glyphicon-remove pull-right delete"></button><button type="button" class="btn btn-primary productPrice pull-right">'+newJSON.price+'</button><h3 class="col-md-6 productName">'+newJSON.item+'</h3><h4 class="col-md-6 productDes">'+newJSON.des+'</h4></div>');
+						$(".appe").append('<div id="'+newJSON.id+'" class="jumbotron col-md-8"class="glyphicon glyphicon-remove pull-right"><button class="glyphicon glyphicon-remove pull-right delete"></button><button type="button" class="btn btn-primary productPrice pull-right">'+newJSON.price+'</button><h3 class="col-md-6 productName">'+newJSON.item+'</h3><h4 class="col-md-6 productDes">'+newJSON.des+'</h4></div>');
 									
 						}
 			});
